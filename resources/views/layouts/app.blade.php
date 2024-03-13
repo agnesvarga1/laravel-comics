d2594c23a0fcf83deedc2381bab633cdb6eccd9a
agnesvarga1 <agikolyok@gmail.com>
2024-03-13 16:45:50 +0100

    added header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,13 +1,24 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-bs-theme="dark">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>
+        @yield('title')
+    </title>
+
     @vite('resources/js/app.js')
-    <title>@yield("")</title>
 </head>
 <body>
- <h1 class="text-center text-success">Mything</h1>
+
+    {{-- header --}}
+     @include('partials.header')
+
+    {{-- main --}}
+
+
+    {{-- footer --}}
+
 </body>
 </html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,14 @@
+<header>
+    <nav>
+      <figure>
+        <img src="{{  Vite::asset('resources/img/dc-logo.png') }}" alt="logo" width="90" />
+      </figure>
+      <ul>
+          <li><a href="#">characters</a></li>
+          <li><a href="#">comics</a></li>
+          <li><a href="#">movies</a></li>
+          <li><a href="#">tv</a></li>
+          <li><a href="#">games</a></li>
+      </ul>
+    </nav>
+  </header>
